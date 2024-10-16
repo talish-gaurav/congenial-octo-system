2a006f730c8de184ea767ece62a4cea7bb700565
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-16 19:50:33 +0100

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,24 @@
+a=10, "of December"
+
+b=30, "of August"
+
+c=3, "of October"
+
+d=29, "of May"
+
+e=1, "of January"
+
+Rohit=input("Enter your name")
+print("His birthday is:", a)
+
+Raj=input("Enter your name")
+print("His birthday is:",b)
+
+Shaunak=input("Enter your name")
+print("His birthday is:",c)
+
+Arjun=input("Enter your name")
+print("His birthday is:",d)
+
+Himaksh=input("Enter your name")
+print("His birthday is:",e)
